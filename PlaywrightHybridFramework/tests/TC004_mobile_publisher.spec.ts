908beef6a53ce11e8b6f6a9c1efc04e9de6bee4b
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-04-08 18:28:25 +0530

    Add files via upload

---
+++ PlaywrightHybridFramework/tests/TC004_mobile_publisher.spec.ts
@@ -0,0 +1,15 @@
+import { test } from "../customFixtures/salesForceFixture"
+
+test(` Mobile Publisher testCase`, async ({ SalesforceLogin, SalesforceHome, SalesforceMobilePublisher }) => {
+    test.info().annotations.push(
+        { type: 'TestCase', description: 'Mobile publisher multiple page' },
+    );
+
+    await SalesforceLogin.salesforceLogin("ADMINLOGIN");
+    await SalesforceLogin.verifyHomeLabel();
+    await SalesforceHome.clickMobilePublisher();
+    await SalesforceMobilePublisher.clickConfirmButton();
+    await SalesforceMobilePublisher.clickProduct();
+    await SalesforceMobilePublisher.clickAgentforce();
+    await SalesforceMobilePublisher.hoverPricing();
+})
